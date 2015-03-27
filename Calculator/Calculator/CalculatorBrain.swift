b4d21fc602c699c2f821e6f5eafba4b6df28ebc7
diveu <ivkitov@gmail.com>
2015-03-27 15:22:52 +0300

    swift calc from stanford course

---
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,158 @@
+import Foundation
+
+class CalculatorBrain
+{
+    private enum Op : Printable
+    {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        case NoOperation(String, () -> Double)
+        case Variable(String)
+        var description : String{
+            get{
+                switch self{
+                case .Operand(let operand):
+                    return "\(operand)"
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .NoOperation(let symbol, _):
+                    return symbol
+                case .Variable(let keyvar):
+                    return keyvar
+                }
+            }
+        }
+    }
+    
+    private var opStack = [Op]()
+    
+    var variableValues = [String:Double]()
+    
+    private var knownOps = [String:Op] ()
+    
+    var descriprion: String{
+        get{ return returnStringEval(opStack).result!
+            
+        }
+    }
+    
+    init ()
+    {
+        func learnOp(op: Op)
+        {
+            knownOps[op.description] = op
+        }
+        learnOp(Op.BinaryOperation("✖️", *))
+        learnOp(Op.BinaryOperation("➗") {$1 / $0})
+        learnOp(Op.BinaryOperation("➕", +))
+        learnOp(Op.BinaryOperation("➖") {$1 - $0})
+        learnOp(Op.UnaryOperation("√", sqrt))
+        learnOp(Op.UnaryOperation("sin", sin))
+        learnOp(Op.UnaryOperation("cos", cos))
+        learnOp(Op.NoOperation("π", {M_PI}))
+
+    }
+    
+    func clearStack(){
+        opStack.removeAll(keepCapacity: false)
+    }
+    
+    private func returnStringEval(ops: [Op]) -> (result: String?, remainingOps: [Op])
+    {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return ("\(operand)", remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandString = returnStringEval(remainingOps)
+                if let operand = operandString.result {
+                    return (op.description + "(" + operand + ")", operandString.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1str = returnStringEval(remainingOps)
+                if let operand1 = op1str.result {
+                    let op2str = returnStringEval(op1str.remainingOps)
+                    if let operand2 = op2str.result {
+                        return (operand1 + op.description + operand2, op2str.remainingOps)
+                    }
+                }
+            case .NoOperation(_, let operation):
+                return (op.description, remainingOps)
+            case .Variable(let keyvar):
+                if let variableIsSet = variableValues[keyvar]{
+                    return ("\(variableIsSet)", remainingOps)
+                }
+                else
+                {
+                    return (nil, remainingOps)
+                }
+            }
+        }
+        return (nil, ops)
+
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            case .NoOperation(_, let operation):
+                return (operation(), remainingOps)
+            case .Variable(let keyvar):
+                if let variableIsSet = variableValues[keyvar]{
+                    return (variableIsSet, remainingOps)
+                }
+                else
+                {
+                    return (nil, remainingOps)
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        //println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    
+    func pushVariable(symbol: String) -> Double?{
+        opStack.append(Op.Variable(symbol))
+        return evaluate()
+    }
+    
+    func pushOperand(operand: Double) -> Double?{
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double?{
+        if let operation = knownOps[symbol]{
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+}
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Иван on 05.03.15.
+//  Copyright (c) 2015 KKK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    @IBOutlet weak var stat: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userEnteredDot = false
+    var brain = CalculatorBrain()
+    var wasCleared = false
+    
+    
+    @IBAction func clearScreen(sender: UIButton) {
+        display.text = "0"
+        stat.text = " "
+        brain.clearStack()
+        wasCleared = true
+    }
+    
+    @IBAction func bSpace(sender: UIButton) {
+        if countElements(display.text!) <= 1
+        {
+            display.text = "0"
+        }
+        else
+        {
+            display.text = dropLast(display.text!)
+        }
+    }
+    
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            if digit == "."
+            {
+                userEnteredDot = true
+            }
+            if !userEnteredDot
+            {
+                display.text = display.text! + digit
+            }
+            else
+            {
+                display.text = "err"
+            }
+        }
+        else
+        {
+            if digit == "."
+            {
+                display.text = "0."
+                userEnteredDot = true
+            }
+            else{
+                display.text = digit
+            }
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber
+        {
+            enter()
+        }
+        if let operation = sender.currentTitle
+        {
+            stat.text = stat.text! + sender.currentTitle! + " = "
+            if let result = brain.performOperation(operation){
+                displayValue = result
+            }
+            else {
+                displayValue = nil
+                display.text = "0"
+            }
+        }
+        
+    }
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
+            
+        }
+        else {
+            displayValue = nil
+            display.text = "0"
+        }
+    }
+    
+    var displayValue: Double! {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            stat.text = stat.text! + display.text! + " "
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+}
+
